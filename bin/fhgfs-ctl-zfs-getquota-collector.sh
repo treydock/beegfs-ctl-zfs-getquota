dfae63112f277fa916ff8579b2e9d25ec9c718ea
Trey Dockendorf <treydock@tamu.edu>
2015-07-10 23:20:08 -0500

    * Fix collector script to correctly use the configured storage target path when fhgfs-storage is using a ZFS filesystem other than 'tank'

--- bin/fhgfs-ctl-zfs-getquota-collector.sh
+++ bin/fhgfs-ctl-zfs-getquota-collector.sh
@@ -31,14 +31,14 @@ for host in $HOSTS; do
   # Get storeStorageDirectory value from fhgfs-storage.conf
   storeStorageDirectory=$(ssh -nq root@${host} "awk '/^storeStorageDirectory/{print \$NF }' /etc/fhgfs/fhgfs-storage.conf")
   # Determine if storeStorageDirectory is a mounted ZFS filesystem
-  filesystem=$(ssh -nq root@${host} "grep '${storeStorageDirectory}' /proc/mounts | awk '{ print $1 }'")
+  filesystem=$(ssh -nq root@${host} "grep '${storeStorageDirectory}' /proc/mounts | awk '{ print \$1 }'")
   if [ -z "$filesystem" ]; then
     QUERY_FILESYSTEM=${ZFS_FILESYSTEM}
   else
     QUERY_FILESYSTEM=${filesystem}
   fi
-  exec_cmd "ssh -nq root@${host} 'zfs userspace -n -H -p -o name,used -s name ${ZFS_FILESYSTEM} 2>/dev/null' >>${USERSPACE_OUTPUT}"
-  exec_cmd "ssh -nq root@${host} 'zfs groupspace -n -H -p -o name,used -s name ${ZFS_FILESYSTEM} 2>/dev/null' >>${GROUPSPACE_OUTPUT}"
+  exec_cmd "ssh -nq root@${host} 'zfs userspace -n -H -p -o name,used -s name ${QUERY_FILESYSTEM} 2>/dev/null' >>${USERSPACE_OUTPUT}"
+  exec_cmd "ssh -nq root@${host} 'zfs groupspace -n -H -p -o name,used -s name ${QUERY_FILESYSTEM} 2>/dev/null' >>${GROUPSPACE_OUTPUT}"
 done
 
 exit 0
